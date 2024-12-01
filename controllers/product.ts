76c33a0fc33b05e0a91b4e59bccf520cee676f52
oshiogifted <oshiogifted2011@gmail.com>
2024-12-01 00:16:56 -0500

    implemented pagination

--- controllers/product.ts
+++ controllers/product.ts
@@ -3,7 +3,18 @@ import productSchema from '../models/product'
 // retrive all products from db
 export const getAllProducts = async (req: any, res: any) => {
 
-  const products = await productSchema.find({})
+  const queryObject = {}
+
+  let result = productSchema.find(queryObject);
+
+   /* skip and limit for pagination */
+   const page = Number(req.query.page) || 1;
+   const limit = Number(req.query.limit) || 6; // default to 6 products
+   const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+ 
+  const products = await result.find({})
 
   res.status(200).json({products, nbHits: products.length})
 }
