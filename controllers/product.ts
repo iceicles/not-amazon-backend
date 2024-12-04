5d49983e9753246ecc7a1618d06da59b6f941e1c
oshiogifted <oshiogifted2011@gmail.com>
2024-12-03 20:24:28 -0500

    filtering by product id path param

--- controllers/product.ts
+++ controllers/product.ts
@@ -18,3 +18,15 @@ export const getAllProducts = async (req: any, res: any) => {
 
   res.status(200).json({products, nbHits: products.length})
 }
+
+
+export const getProductById = async (req: any, res: any) => {
+  const { id: productId } = req.params;
+  const product = await productSchema.findOne({ _id: productId }); // or findById(productId)
+  if (!product) {
+    // if product ID is right length but doesn't exist
+    // next passes control to the middleware in this case, error-handler middleware
+    return res.send('no product with id')// next(createCustomError(`No product with id : ${productId}`, 404));
+  }
+  res.status(200).json({ product: [product] });
+};
--- routes/products.ts
+++ routes/products.ts
@@ -2,9 +2,10 @@ import express from 'express';
 
 const router = express.Router()
 
-import { getAllProducts } from '../controllers/product';
+import { getAllProducts, getProductById } from '../controllers/product';
 
 // '/' => '/api/v1/products'
 router.route('/').get(getAllProducts)
+router.route('/:id').get(getProductById)
 
 export default router;
